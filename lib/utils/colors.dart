f05e4f6772206cafa49e11397d2a510502e7de96
Arif Rahman <68379379+Arifrhmn2743@users.noreply.github.com>
2022-07-07 11:42:15 +0700

    Update colors.dart

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ColorPallet {
-  static const Color darkPurple = Color(0xff2a3d66);
-  static const Color darkLightPurple = Color(0xff5d54a4);
-  static const Color lightPurple = Color(0xff9d65c9);
-  static const Color pastelPurple = Color(0xffd789d7);
   static const Color primary = Color(0xff1a237e);
   static const Color light = Color(0xff534bae);
   static const Color dark = Color(0xff000051);
