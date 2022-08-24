1dd9169895cb2d19250f71105b57505a979370c8
Arif Rahman <68379379+Arifrhmn2743@users.noreply.github.com>
2022-08-24 15:51:44 +0700

    update

--- lib/screen/firstScreen/screen1.dart
+++ lib/screen/firstScreen/screen1.dart
@@ -25,35 +25,6 @@ class _Screen1State extends State<Screen1> {
   Widget build(BuildContext context) {
     return Consumer<MainProvider>(builder: (context, value, child) {
       return Scaffold(
-          floatingActionButton: FloatingActionButton.extended(
-            backgroundColor: ColorPallet.dark,
-            onPressed: () {
-              if (value.rocketName == "") {
-                Get.defaultDialog(
-                  title: "Warning!",
-                  middleText: "Rocket name cannot be empty ",
-                );
-              } else {
-                Get.to(Screen2());
-              }
-            },
-            label: Row(
-              children: [
-                Text(
-                  "Next Step",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15.0,
-                  ),
-                ),
-                SizedBox(width: 10),
-                Icon(
-                  Icons.arrow_forward,
-                  color: Colors.white,
-                )
-              ],
-            ),
-          ),
           backgroundColor: ColorPallet.primary,
           body: SafeArea(
               child: Column(
@@ -65,45 +36,6 @@ class _Screen1State extends State<Screen1> {
                     'https://assets4.lottiefiles.com/packages/lf20_yznfvm5w.json',
                     fit: BoxFit.fill,
                   )),
-              Expanded(
-                  child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 20),
-                      Text(
-                        "The universe is a wonderful place",
-                        style: bigBoldWhiteTextStyle,
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        "Before we begin our journey, Please name our rocket ship !",
-                        style: bigBoldWhiteTextStyle,
-                      ),
-                      SizedBox(height: 20),
-                      Container(
-                        height: 56,
-                        padding: EdgeInsets.fromLTRB(16, 3, 16, 6),
-                        decoration: raisedDecoration,
-                        child: Center(
-                          child: TextField(
-                            autofocus: true,
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Rocket name",
-                            ),
-                            onChanged: (v) {
-                              value.setRocketName(v);
-                            },
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ))
             ],
           )));
     });
